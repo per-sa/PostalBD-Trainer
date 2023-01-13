cb37cc987b02ad0fea0b21d3d02d841211231cd2
matheusclmb <ccolombomatheus@gmail.com>
2023-01-13 03:11:57 +0000

    Feat: Added .ICO

--- PostalBD Trainer/Form1.Designer.cs
+++ PostalBD Trainer/Form1.Designer.cs
@@ -140,6 +140,7 @@
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "POSTAL Brain Damage Trainer";
             this.Load += new System.EventHandler(this.Form1_Load);
